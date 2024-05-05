265bb40ae45d744dfd38e95f14e07ec77e7d6f9d
mapersiani <122496033+mapersiani@users.noreply.github.com>
2024-05-05 16:50:14 -0700

    updates volume slider

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,7 @@
 
 window.addEventListener('DOMContentLoaded', init);
 const jsConfetti = new JSConfetti()
+
 function init() {
   const selectElement = document.querySelector("select");
   const image = document.querySelector("img");
@@ -11,7 +12,9 @@ function init() {
     image.src = `assets/images/${event.target.value}.svg`;
     audio.src = `assets/audio/${event.target.value}.mp3`;
   });
+
   const button = document.querySelector("button");
+
   button.addEventListener("click", (event) => {
     let audio = document.querySelector("audio");
     audio.play();
@@ -22,4 +25,25 @@ function init() {
       })
     }
   });
+
+  const volume = document.querySelector("input");
+  const volumeImage = document.querySelector("div img");
+
+  volume.addEventListener("input", (event) => {
+    audio.volume = event.target.value / 100;
+    if (event.target.value == 0) {
+      volumeImage.src = "assets/icons/volume-level-0.svg";
+    }
+    else if (event.target.value < 33) {
+      volumeImage.src = "assets/icons/volume-level-1.svg";
+    }
+    else if (event.target.value < 67) {
+      volumeImage.src = "assets/icons/volume-level-2.svg";
+    }
+    else {
+      volumeImage.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+
 }
